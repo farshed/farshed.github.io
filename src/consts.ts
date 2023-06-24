27796a84958b4371a659ff9da704d1b2b8d076cb
farshed <faisalarshed28@gmail.com>
2023-06-25 02:35:44 +0500

    Add social icons

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'Faisal Arshed';
 export const SITE_DESCRIPTION = 'Faisal Arshed is a software engineer based in Lahore, Pakistan.';
+export const EMAIL = 'mail@farshed.me';
