65cbe720e3c018077c1cb73a0354e945b36e2b75
farshed <faisalarshed28@gmail.com>
2023-06-25 23:23:45 +0500

    Update

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Faisal Arshed';
 export const SITE_DESCRIPTION = 'Faisal Arshed is a software engineer based in Lahore, Pakistan.';
 export const EMAIL = 'mail@farshed.me';
