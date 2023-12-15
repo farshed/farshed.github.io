aa9aa7dd85dd94c02a66c951dfc0c4b2ed7dca26
farshed <faisalarshed28@gmail.com>
2023-12-15 21:12:22 +0500

    Add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import rehypeExternalLinks from 'rehype-external-links';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://farshed.me',
   compressHTML: true,
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   markdown: {
-    rehypePlugins: [[rehypeExternalLinks, { target: ['_blank'] }]],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: ['_blank']
+    }]],
     shikiConfig: {
       theme: 'dracula',
       langs: ['js', 'rs']
