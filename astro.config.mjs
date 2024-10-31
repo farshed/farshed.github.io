e4de875bf6aed3950c5148651b9c1449b727da30
farshed <faisalarshed28@gmail.com>
2024-10-31 09:23:24 +0500

    Add tailwind and update resume

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,25 +5,21 @@ import sitemap from '@astrojs/sitemap';
 import rehypeExternalLinks from 'rehype-external-links';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://farshed.me',
   compressHTML: true,
   build: {
     inlineStylesheets: `never`
   },
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), tailwind()],
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [
-      rehypeKatex,
-      [
-        rehypeExternalLinks,
-        {
-          target: ['_blank']
-        }
-      ]
-    ],
+    rehypePlugins: [rehypeKatex, [rehypeExternalLinks, {
+      target: ['_blank']
+    }]],
     shikiConfig: {
       theme: 'dracula'
       // https://shikiji.netlify.app/guide/dual-themes#light-dark-dual-themes
