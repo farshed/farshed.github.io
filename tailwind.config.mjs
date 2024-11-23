4e1dc32231d684ea12d816c78e3da51fe775b421
farshed <faisalarshed28@gmail.com>
2024-11-24 03:26:15 +0500

    Update selection color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        customBlue: '#3562f3'
+      },
       typography: {
         DEFAULT: {
           css: {
