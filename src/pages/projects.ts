040b12aac506724e89b561db81b63adbfb948093
farshed <faisalarshed28@gmail.com>
2024-11-23 16:32:38 +0500

    Add projects

---
+++ src/pages/projects.ts
@@ -0,0 +1,79 @@
+const projects = [
+  {
+    title: 'Soundspice',
+    url: 'https://github.com/farshed/SoundSpice-mobile',
+    description:
+      'Minimalist music player for Android. Featured in Beebom Best Open-source Android Apps of 2020.',
+    stars: '400+'
+  },
+  {
+    title: 'Duofolio',
+    url: 'https://github.com/farshed/duofolio',
+    description:
+      'Ebook reader to help language learners effectively acquire new vocabulary in their target language.',
+    stars: '450+'
+  },
+  {
+    title: 'DeepGaze',
+    description:
+      'Experiment in realtime object detection and image classification on edge devices using Tensorflow.',
+    url: 'https://github.com/farshed/deepgaze'
+  },
+  {
+    title: 'Genius++',
+    url: 'https://github.com/farshed/genius-lyrics-api',
+    description: 'Library for aggregating lyrics and song metadata from Genius API and website.',
+    stars: '200+'
+  },
+  {
+    title: 'Zusammen',
+    description: 'CLI-based parallel downloader using Rust and Tokio.',
+    url: 'https://github.com/farshed/zusammen'
+  },
+  {
+    title: 'Pennywise',
+    description: 'Robust banking ledger backend.',
+    url: 'https://github.com/farshed/pennywise'
+  }
+];
+
+export default projects;
+
+// const projects = [
+//   {
+//     title: 'Soundspice',
+//     url: 'https://github.com/farshed/SoundSpice-mobile',
+//     description:
+//       'A light-weight and minimalist music player for Android. Featured by Beebom as one of the best open-source Android apps of 2020. It introduced in-app lyrics years before Spotify.',
+//     stars: '400+'
+//   },
+//   {
+//     title: 'Duofolio',
+//     url: 'https://github.com/farshed/duofolio',
+//     description:
+//       'An ebook reader to help language learners effectively acquire new vocabulary in their target language.',
+//     stars: '450+'
+//   },
+//   {
+//     title: 'DeepGaze',
+//     description:
+//       'An experiment in realtime object detection & classification on mobile devices using Tensorflow.',
+//     url: 'https://github.com/farshed/deepgaze'
+//   },
+//   {
+//     title: 'Genius Metadata Scraper',
+//     url: 'https://github.com/farshed/genius-lyrics-api',
+//     description: 'A library for aggregating lyrics and song metadata from genius.com.',
+//     stars: '200+'
+//   },
+//   {
+//     title: 'Zusammen',
+//     description: 'A CLI-based parallel downloader using Rust and Tokio.',
+//     url: 'https://github.com/farshed/zusammen'
+//   },
+//   {
+//     title: 'Pennywise',
+//     description: 'A robust banking ledger.',
+//     url: 'https://github.com/farshed/pennywise'
+//   }
+// ];
